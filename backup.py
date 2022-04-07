f34070c82d0a54f738596f3587e0bc6c04ad1ec7
Artur <73757154+Ex6linz@users.noreply.github.com>
2022-04-07 10:27:56 +0200

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,18 +1,20 @@
-import datetime
-import shutil
 from datetime import date
-import glob
+import shutil
 
-path = '/backup'
-
-src_backup_dir = '/redmine'
-dst_backup = '/backup'
+src_back_dir = '/redmine'
+dst_backup_dir = '/backup'
 
 try:
-    shutil.copytree(src_backup_dir,dst_backup)
+    today = date.today()
+    date_format = today.strftime("%d_%b_%y_")
+    dst_backup_dir = dst_backup_dir + date_format
+    shutil.copytree(src_back_dir,dst_backup_dir)
     print("File copied succesfully")
 
 except shutil.SameFileError:
-    print("File does not exist or src and dst represents the same files ")
+    
+    print("File does not exist or src and dst represents the same files")
+
 except:
-    print("Error occurred while copying file")
+    
+    print("Error occurred while copying file")        
