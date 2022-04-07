633264cf4638d5829545701720f810be854bd08e
Artur <73757154+Ex6linz@users.noreply.github.com>
2022-04-07 09:39:46 +0200

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,18 @@
+import datetime
+import shutil
+from datetime import date
+import glob
+
+path = '/backup'
+
+src_backup_dir = '/redmine'
+dst_backup = '/backup'
+
+try:
+    shutil.copy(src_backup_dir,dst_backup)
+    print("File copied succesfully")
+
+except shutil.SameFileError:
+    print("File does not exist or src and dst represents the same files ")
+except:
+    print("Error occurred while copying file")
