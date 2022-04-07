eae03f85020bf59385c895d0cd1cdeb3e95403c8
Artur <73757154+Ex6linz@users.noreply.github.com>
2022-04-07 09:38:19 +0200

    Create Deldir.py

---
+++ Deldir.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+import glob
+
+path = '/backup'
+
+today.datetime.datetime.today()
+os.chdir(path)
+
+for root, directories, files in os.walk(path,topdown=False):
+    for name in files:
+        t.os.stat(os.path.join(root,name))[8]
+        filetime = datetime.datetime.fromtimestamp(t)-today
+
+        if filetime.days <= -7:
+            print(os.path.join(root,name), filetime.days)
+            os.remove(os.path.join(root,name))
